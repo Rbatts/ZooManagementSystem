c48b9b0900c74ad4f3249e39ab9783c556e43cc6
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-11-09 12:26:46 +0000

    working on mucking out animals as well as grooming and feeding

--- Zoo.ConsoleApp/Program.cs
+++ Zoo.ConsoleApp/Program.cs
@@ -22,6 +22,7 @@ namespace Zoo.ConsoleApp
       };
       var smallAnimals = new ISmallAnimal[] {
         new Rabbit(new DateTime(2014, 1, 1)),
+        new GuineaFowl(new DateTime(2017, 11, 09)), 
       };
       var animals = largeAnimals.Union<IAnimal>(smallAnimals).ToList();
 
